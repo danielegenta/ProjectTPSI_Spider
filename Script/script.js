20fba32231deea1693385f88ab25ba5837f94748
danielegenta <danielegenta@icloud.com>
2015-01-29 18:16:16 +0100

    0.1v1
    -Abbozzo HTML
    -Introduzione documento cose da fare

--- Script/script.js
+++ Script/script.js
@@ -1,5 +1,30 @@
+//Variabili globali
 
+
+//
 $(document).ready(function()
 {	
-	alert("prova");
+	creazioneTopDecks();
 });
+
+//Creazione dinamica div che compongono il mazzo superiore
+function creazioneTopDecks()
+{
+	var i=0;
+	for (i = 0; i < 10; i++) 
+	{	
+    	var classeDiv = "topDecks";
+        var contenutoDiv;
+        //if(i == 15) 
+            contenutoDiv = "p";
+        //else
+        	//contenutoDiv = i+1;
+        var cella = $('<div />');
+        cella.attr("id",i);
+        cella.attr("onclick","clickCell(" +i+ ")");
+        cella.addClass(classeDiv);
+        cella.html(contenutoDiv);
+        //Appendo al padre i div figli
+        $("#topDeck").append(cella);
+    }
+}
