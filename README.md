52cd1f3a8e8f1c0a20beaca3ea6c97bc93ae6a8b
danielegenta <danielegenta@icloud.com>
2015-02-04 18:41:09 +0100

    0.4v2
    -Separazione drop e scoperta
    -Parziale documentazione del codice
    -Parziale pulizia del codice
    -Abbozzo spostamento multiplo

--- README.md
+++ README.md
@@ -13,5 +13,6 @@ Major updates:
 0.1 struttura dei due mazzi e posizionamento
 0.2 aggiunta carte coperte e miglioramento struttura HTML
 0.3 finita aggiunta carte coperte, ultima fila scoperta e distribuzione carte da mazzo inferiore
+0.4 spostamento carte singolo e scoperta di eventuali carte coperte
 
 //
--- Script/script.js
+++ Script/script.js
@@ -24,7 +24,7 @@ $(document).ready(function()
 	});
 	drag();
 	drop();
-	
+	//multipleDrag();
 });
 
 //Funzione che gestisce il nome del nomeGiocatore
@@ -152,13 +152,14 @@ function generaNumeroCasuale()
 	
 }
 
-//rende l'ultima riga draggable
+//rende l'ultima riga draggable (per ogni riga ottiene la lunghezza e gli applica tale proprietà)
 function drag()
 {
+	var count;
 	for(var i=0;i<10;i++)
 	{
-	var count = $("#"+i).children().length;
-	$("#"+$("#"+i).children().eq(count-1).attr("id")).draggable({revert:true,revertDuration:200});
+		count = $("#"+i).children().length;
+		$("#"+$("#"+i).children().eq(count-1).attr("id")).draggable({revert:true});//revertDuration:200});
 	}
 }
 
@@ -166,33 +167,77 @@ function drag()
 //rende tutti i mazzi droppabili
 function drop()
 {
-	for(var i=0;i<10;i++)
+	var i;
+	for(i=0;i<10;i++)
 	{
-	$("#"+i ).droppable({
-	 tolerance: "touch",
-     drop: function( event, ui ){ 
-		var idDrop=$(this).attr("id");//id colonna
-		var idDrag=ui.draggable.attr("id");//id carta spostata
-		var count=$("#"+idDrop).children().length;//lunghezza nuova colonna
-		var lastId=$("#"+idDrop).children().eq(count-1).attr("id");//id ultima carta nuova colonna
-		var confronta1=parseInt($("#"+lastId).find("img").attr("name"))-1;//valore ultima carta nuova colonna-1
-		var confronta2=$("#"+idDrag).find("img").attr("name");//valore carta spostata
-		if(confronta1==confronta2)
-		{
-		var nID=count+""+idDrop;//id della carta nella nuova colonna
-		var oldID = idDrag.split("");//id vecchia colonna
-		$( "#"+idDrag).draggable({revert:false});
-		$("#"+oldID).remove(":last-child");
-		$("#"+idDrop).append($("#"+idDrag));
-		$("#"+idDrag).attr("id",nID);
-		$( "#"+nID).draggable({revert:true});
-		var oldCount=$("#"+oldID[1]).children().length;//lunghezza vecchia colonna
-		var scopri=$("#"+oldID[1]).children().eq(oldCount-1).attr("id");//id ultima carta vecchia colonna
-		$("#"+scopri).attr("value");//gira la carta
-		$("#"+scopri).html('<img name='+$("#"+scopri).attr("value")+' src="img/'+$("#"+scopri).attr("value")+'.jpg">');
-		$("#"+scopri).draggable({revert:true,revertDuration:200});		
-		}		
-      }
-    });
+		$("#"+i ).droppable({
+			tolerance: "touch",
+     		drop: function( event, ui ){ 
+				var idDrop=$(this).attr("id");//id colonna
+				var idDrag=ui.draggable.attr("id");//id carta spostata
+				var count=$("#"+idDrop).children().length;//lunghezza nuova colonna
+				var lastId=$("#"+idDrop).children().eq(count-1).attr("id");//id ultima carta nuova colonna
+				var confronta1=parseInt($("#"+lastId).find("img").attr("name"))-1;//valore ultima carta nuova colonna-1
+				var confronta2=$("#"+idDrag).find("img").attr("name");//valore carta spostata
+				if(confronta1==confronta2)
+				{
+					var nID=count+""+idDrop;//id della carta nella nuova colonna
+					var oldID = idDrag.split("");//id vecchia colonna
+					$( "#"+idDrag).draggable({revert:false});
+					$("#"+oldID).remove(":last-child");
+					$("#"+idDrop).append($("#"+idDrag));
+					$("#"+idDrag).attr("id",nID);
+					$( "#"+nID).draggable({revert:true});
+					scopri(oldID);		
+				}		
+      		}
+    	});
 	}
+}
+
+//Funzione che si occupa di scoprire una carta qualora quella prima di lei venga spostata ed essa sia coperta
+function scopri(oldID) 
+{
+	var oldCount=$("#"+oldID[1]).children().length;//lunghezza vecchia colonna
+					var scopri=$("#"+oldID[1]).children().eq(oldCount-1).attr("id");//id ultima carta vecchia colonna
+					//$("#"+scopri).attr("value");
+					//Gira la carta
+					$("#"+scopri).html('<img name='+$("#"+scopri).attr("value")+' src="img/'+$("#"+scopri).attr("value")+'.jpg">');
+					$("#"+scopri).draggable({revert:true,revertDuration:200});
+					multipleDrag();
+}
+
+//Funzione aggiornata 
+function multipleDrag()
+{
+	var scala=1;
+	var valoreScala=0;
+	var contenuto=0;
+	var count= $("#"+0).children().length;
+	//prima lettura fuori ciclo della carta scoperta
+	contenuto=$("#"+(count-1)+"0").children().attr("name");
+	valoreScala=parseInt(contenuto);
+	valoreScala+=1;
+	for (i=count-2; i>=0; i--)
+	{
+		contenuto=$("#"+i+"0").children().attr("name");
+		//La carta è scoperta
+		if (contenuto != undefined)
+		{
+			if (parseInt(contenuto)==(valoreScala))
+			{
+			valoreScala=parseInt(contenuto);
+			valoreScala+=1;
+			}
+			else
+				scala=0;
+		}
+	}
+	if (scala==1)
+			
+		{
+			
+			$("#50").append($("#"+0).children().eq(count-1));
+			$("#"+0).remove(":last-child");
+		}
 }
